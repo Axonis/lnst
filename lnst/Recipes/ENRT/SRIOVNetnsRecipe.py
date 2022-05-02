6082a8a27c9dd499c4c4160cec1636c41b709be3
Jozef Urbanovsky <jurbanov@redhat.com>
2022-05-02 12:25:56 +0200

    Recipes.ENRT.SRIOVNetnsRecipe: add new SRIOV recipe

---
+++ lnst/Recipes/ENRT/SRIOVNetnsRecipe.py
@@ -0,0 +1,147 @@
+from lnst.Common.Parameters import Param
+from lnst.Common.IpAddress import ipaddress
+from lnst.Controller import HostReq, DeviceReq, RecipeParam
+from lnst.Controller.NetNamespace import NetNamespace
+from lnst.Recipes.ENRT.BaremetalEnrtRecipe import BaremetalEnrtRecipe
+from lnst.RecipeCommon.Ping.PingEndpoints import PingEndpoints
+from lnst.Recipes.ENRT.ConfigMixins.OffloadSubConfigMixin import (
+    OffloadSubConfigMixin,
+)
+from lnst.Recipes.ENRT.ConfigMixins.CommonHWSubConfigMixin import (
+    CommonHWSubConfigMixin,
+)
+
+
+class SRIOVNetnsRecipe(
+    CommonHWSubConfigMixin, OffloadSubConfigMixin, BaremetalEnrtRecipe
+):
+    """
+    This recipe implements Enrt testing for a simple network scenario that looks
+    as follows
+
+    .. code-block:: none
+
+                    +--------+
+             +------+ switch +-----+
+             |      +--------+     |
+          +--+-+                 +-+--+
+        +-|eth0|-+             +-|eth0|-+
+        | +----+ |             | +----+ |
+        | host1  |             | host2  |
+        +--------+             +--------+
+
+    All sub configurations are included via Mixin classes.
+
+    The actual test machinery is implemented in the :any:`BaseEnrtRecipe` class.
+    """
+    host1 = HostReq()
+    host1.eth0 = DeviceReq(label="net1", driver=RecipeParam("driver"))
+
+    host2 = HostReq()
+    host2.eth0 = DeviceReq(label="net1", driver=RecipeParam("driver"))
+
+    offload_combinations = Param(default=(
+        dict(gro="on", gso="on", tso="on", tx="on", rx="on"),
+        dict(gro="off", gso="on", tso="on", tx="on", rx="on"),
+        dict(gro="on", gso="off", tso="off", tx="on", rx="on"),
+        dict(gro="on", gso="on", tso="off", tx="off", rx="on"),
+        dict(gro="on", gso="on", tso="on", tx="on", rx="off")))
+
+    def test_wide_configuration(self):
+        """
+        Test wide configuration for this recipe involves just adding an IPv4 and
+        IPv6 address to the matched eth0 nics on both hosts.
+
+        host1.eth0 = 192.168.101.1/24 and fc00::1/64
+
+        host2.eth0 = 192.168.101.2/24 and fc00::2/64
+        """
+        host1, host2 = self.matched.host1, self.matched.host2
+        configuration = super().test_wide_configuration()
+        configuration.test_wide_devices = []
+
+        for i, host in enumerate([host1, host2]):
+            host.run(f"devlink dev eswitch set pci/{host.eth0.bus_info} mode switchdev")
+            host.run(f"echo 1 > /sys/class/net/{host.eht0.name}/device/sriov_numvfs")
+
+            vf_ifname = dict(ifname=f"{host.eth0.name}v0")
+            host.map_device(f"{vf_ifname}")
+
+            host.newns = NetNamespace(f"host{i}")
+            host.newns.eth0 = host.eth0
+
+            host.newns.eth0.ip_add(ipaddress("192.168.101." + str(i+1) + "/24"))
+            host.newns.eth0.ip_add(ipaddress("fc00::" + str(i+1) + "/64"))
+            host.newns.eth0.up()
+            configuration.test_wide_devices.append(host.newns.eth0)
+
+        self.wait_tentative_ips(configuration.test_wide_devices)
+
+        return configuration
+
+    def generate_test_wide_description(self, config):
+        """
+        Test wide description is extended with the configured addresses
+        """
+        desc = super().generate_test_wide_description(config)
+        desc += [
+            "Configured {}.{}.ips = {}".format(
+                dev.host.hostid, dev.name, dev.ips
+            )
+            for dev in config.test_wide_devices
+        ]
+        return desc
+
+    def test_wide_deconfiguration(self, config):
+        ""  # overriding the parent docstring
+        del config.test_wide_devices
+
+        super().test_wide_deconfiguration(config)
+
+    def generate_ping_endpoints(self, config):
+        """
+        The ping endpoints for this recipe are simply the two matched NICs:
+
+        host1.eth0 and host2.eth0
+
+        Returned as::
+
+            [PingEndpoints(self.matched.host1.eth0, self.matched.host2.eth0)]
+        """
+        return [PingEndpoints(self.matched.host1.newns.eth0, self.matched.host2.newns.eth0)]
+
+    def generate_perf_endpoints(self, config):
+        """
+        The perf endpoints for this recipe are simply the two matched NICs:
+
+        host1.eth0 and host2.eth0
+
+        Returned as::
+
+            [(self.matched.host1.eth0, self.matched.host2.eth0)]
+        """
+        return [(self.matched.host1.newns.eth0, self.matched.host2.newns.eth0)]
+
+    @property
+    def pause_frames_dev_list(self):
+        return [self.matched.host1.newns.eth0, self.matched.host2.newns.eth0]
+
+    @property
+    def offload_nics(self):
+        return [self.matched.host1.newns.eth0, self.matched.host2.newns.eth0]
+
+    @property
+    def mtu_hw_config_dev_list(self):
+        return [self.matched.host1.newns.eth0, self.matched.host2.newns.eth0]
+
+    @property
+    def coalescing_hw_config_dev_list(self):
+        return [self.matched.host1.newns.eth0, self.matched.host2.newns.eth0]
+
+    @property
+    def dev_interrupt_hw_config_dev_list(self):
+        return [self.matched.host1.newns.eth0, self.matched.host2.newns.eth0]
+
+    @property
+    def parallel_stream_qdisc_hw_config_dev_list(self):
+        return [self.matched.host1.newns.eth0, self.matched.host2.newns.eth0]
